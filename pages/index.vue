5ec42eb86f6045c7cb01c73847c814d77b318d3d
yusk-ishii <y.retro404@gmail.com>
2024-11-22 16:36:15 +0900

    feat: 階層を修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template>
     <h1>レイアウト検証</h1>
     <ul>
-        <li><NuxtLink to="/page-750-fixed">750px固定</NuxtLink></li>
+        <li><NuxtLink to="/layouts/page-750-fixed">750px固定</NuxtLink></li>
     </ul>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
