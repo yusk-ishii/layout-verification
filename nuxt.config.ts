22b7142e74b055a3db7ddd0b16d7bd7aedc6d498
yusk-ishii <y.retro404@gmail.com>
2024-11-22 16:58:09 +0900

    baseURL変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/layout-verification/'
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   css: [
