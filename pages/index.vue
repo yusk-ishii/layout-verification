5bbe308f43690affaac1668ea2ad9bca475d6de7
yusk-ishii <y.retro404@gmail.com>
2024-11-22 16:38:26 +0900

    feat: aタグ追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
     <h1>レイアウト検証</h1>
     <ul>
-        <li><NuxtLink to="/layouts/page-750-fixed">750px固定</NuxtLink></li>
+        <li>
+            <NuxtLink to="/layouts/page-750-fixed">750px固定</NuxtLink>
+            <a href="/layouts/page-750-fixed">750px固定</a>
+        </li>
     </ul>
 </template>
